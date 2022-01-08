3bb6282d49482d4b5c4a1be8965f16148a293d9a
Tcodingg <princetlive@yahoo.com>
2022-01-08 15:32:48 -0500

    store input datas in state

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,53 @@
+import { useState } from "react";
 import "./Form.scss";
 
 const Form = () => {
+   const [input, setInput] = useState({
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+   });
+
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      setInput({
+         ...input,
+         [name]: value,
+      });
+   };
+   console.log(input);
    return (
       <form>
-         <input type="text" name="name" placeholder="Name" />
-         <input type="text" name="name" placeholder="Email" />
-         <input type="text" name="name" placeholder="Subject" />
+         <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={input.name}
+         />
+         <input
+            onChange={handleChange}
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={input.email}
+         />
+         <input
+            onChange={handleChange}
+            type="text"
+            name="phone"
+            placeholder="Phone"
+            value={input.phone}
+         />
          <textarea
-            name="textarea"
+            onChange={handleChange}
+            name="message"
             id=""
             cols="30"
             rows="10"
             placeholder="Message"
+            value={input.message}
          ></textarea>
          <button>Submit</button>
       </form>
