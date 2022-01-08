2867a2508d9effe45574f653de1d8705308f9ca1
Tcodingg <princetlive@yahoo.com>
2022-01-08 17:03:59 -0500

    validate input values

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Form.scss";
 
 const Form = () => {
@@ -9,6 +9,11 @@ const Form = () => {
       message: "",
    });
 
+   const [isValidName, setIsValidName] = useState(null);
+   const [isValidEmail, setIsValidEmail] = useState(null);
+   const [isValidPhone, setIsValidPhone] = useState(null);
+
+   // save input values
    const handleChange = (e) => {
       const { name, value } = e.target;
       setInput({
@@ -16,7 +21,75 @@ const Form = () => {
          [name]: value,
       });
    };
-   console.log(input);
+
+   let phone = input.phone.toString().replace(/\D/g, "").substring(0, 10);
+   const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+   // display phone input value
+   const [phoneValue, setPhoneValue] = useState("");
+   useEffect(() => {
+      let input = phone;
+
+      var size = input.length;
+      if (size > 0) {
+         input = "(" + input;
+      }
+      if (size > 3) {
+         input = input.slice(0, 4) + ") " + input.slice(4);
+      }
+      if (size > 6) {
+         input = input.slice(0, 9) + "-" + input.slice(9);
+      }
+      return setPhoneValue(input);
+   }, [phone]);
+   useEffect(() => {
+      let input = phone;
+
+      var size = input.length;
+      if (size > 0) {
+         input = "(" + input;
+      }
+      if (size > 3) {
+         input = input.slice(0, 4) + ") " + input.slice(4);
+      }
+      if (size > 6) {
+         input = input.slice(0, 9) + "-" + input.slice(9);
+      }
+      return setPhoneValue(input);
+   }, [phone]);
+
+   console.log(isValidEmail, isValidName, isValidPhone);
+
+   useEffect(() => {
+      function validateInput() {
+         // validate name
+         input.name.length === 0
+            ? setIsValidName(null)
+            : input.name.length > 4
+            ? setIsValidName(true)
+            : setIsValidName(false);
+
+         // validate phone
+         phoneValue.length === 0
+            ? setIsValidPhone(null)
+            : phoneValue.length === 14
+            ? setIsValidPhone(true)
+            : setIsValidPhone(false);
+         // validate email
+         input.email.length === 0
+            ? setIsValidEmail(null)
+            : regexp.test(input.email)
+            ? setIsValidEmail(true)
+            : setIsValidEmail(false);
+      }
+      validateInput();
+   }, [input.name, input.phone, input.email, phoneValue]);
+
+   // handle submit
+   const handleSubmit = (e) => {
+      e.preventDefault();
+   };
    return (
       <form>
          <input
@@ -38,7 +111,7 @@ const Form = () => {
             type="text"
             name="phone"
             placeholder="Phone"
-            value={input.phone}
+            value={phoneValue}
          />
          <textarea
             onChange={handleChange}
@@ -49,7 +122,7 @@ const Form = () => {
             placeholder="Message"
             value={input.message}
          ></textarea>
-         <button>Submit</button>
+         <button onClick={handleSubmit}>Submit</button>
       </form>
    );
 };
